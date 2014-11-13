e584dd8aa45b2e37a44f41523578b5ba72599513
altescape <hello@michaelwatts.me>
2014-11-13 16:58:55 +0000

    tidied based on others exercises

--- hamming.rb
+++ hamming.rb
@@ -1,8 +1,25 @@
 class Hamming
 
-  def self.compute(strand1, strand2)
+  attr_accessor :strand_a, :strand_b
 
-    (strand1 == strand2) ? 0 : 1
+  class << self
+
+    def compute(strand_a, strand_b)
+      @strand_a, @strand_b = strand_a, strand_b
+      count_mutations
+    end
+
+    private
+
+    def strand_length
+      [@strand_a.length, @strand_b.length].min
+    end
+
+    def count_mutations
+      mutation_count = 0
+      strand_length.times { |i| mutation_count += 1 unless @strand_a[i] == @strand_b[i] }
+      mutation_count
+    end
 
   end
 
